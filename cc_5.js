144310822db8cf1948067708055237fce5185ddf
Carmen Canals <canalsc@usf.edu>
2026-02-16 17:18:34 -0500

    created an array of 3 employee objects

---
+++ cc_5.js
@@ -0,0 +1,19 @@
+
+//Array of 3 employees
+let employees = [ 
+    { 
+        name: "Hank Green", 
+        hourlyRate:  20, 
+        hoursWorked: 40
+    }, 
+    { 
+        name: "Jane Doe", 
+        hourlyRate: 19,
+        hoursWorked: 40
+    }, 
+    { 
+        name: "Bill Nye", 
+        hourlyRate: 22, 
+        hoursWorked: 40
+    }
+]; 
