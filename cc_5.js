dccf2c4e56d7d1ddb5c706ae49455713e723a596
Carmen Canals <canalsc@usf.edu>
2026-02-18 14:04:31 -0500

    created four functions: base pay, overtime pay, taxes, processing payroll

--- cc_5.js
+++ cc_5.js
@@ -4,12 +4,12 @@ let employees = [
     { 
         name: "Hank Green", 
         hourlyRate:  20, 
-        hoursWorked: 40
+        hoursWorked: 70
     }, 
     { 
         name: "Jane Doe", 
         hourlyRate: 19,
-        hoursWorked: 40
+        hoursWorked: 60
     }, 
     { 
         name: "Bill Nye", 
@@ -17,3 +17,28 @@ let employees = [
         hoursWorked: 40
     }
 ]; 
+
+//Function that returns pay for up to 40 hours 
+function calculateBasePay(rate, hours) { 
+    if (hours > 40) {
+        hours = 40
+    }
+    return rate * hours
+};
+
+function calculateOverTimePay(rate, hours) { 
+    if (hours > 40) {
+        rate * 1.5;
+    }
+    return rate * hours
+}; 
+
+function calculateTaxes(grossPay) { 
+    return (hours * rate) * 0.85;
+}; 
+
+function processPayroll(employees) { 
+    console.log(`Name: ${employees.name} | Base Pay: ${calculateBasePay} | Over Time Pay: ${calculateOverTimePay} | Gross Pay ${} | Net Pay ${calculateTaxes}`)
+}; 
+
+processPayroll(employees);
